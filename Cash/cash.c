c64521905b045c7160c1a12f1b546eab528aa31d
stormvansoldt <stormvansoldt94@gmail.com>
2018-11-27 21:14:32 -0700

    updated documentation

--- Cash/cash.c
+++ Cash/cash.c
@@ -3,12 +3,12 @@
 #include <cs50.h>
 
 /*
- * Here we are taking a user-defined input which will be the amount of change they are owed.
- * The program will then output the least amount of coins needed to give the customer that 
- * amount of change. 
+ * Here we are taking a user-defined input which will be the amount of change 
+ * they are owed. The program will then output the least amount of coins needed 
+ * to give the customer that amount of change. 
  *
- * For example, say the user input is 0.41 ($0.41). The program will output '4' (one quarter,
- * one dime, one nickel, and one penny).
+ * For example, say the user input is 0.41 ($0.41). The program will output '4' 
+ * (one quarter, one dime, one nickel, and one penny).
  */
 
 int main(void)
@@ -18,11 +18,11 @@ int main(void)
 	int result = 0;
 	float change = -0.01;
 
-    while (change <= 0.0)
+    do
     {
  	    change = get_float("Enter amount of cash owed: ");
 	    cents = round(change * 100);       
-    }
+    } while (change <= 0.0);
 
 	while (cents != 0)
 	{

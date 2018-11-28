7d5fff0ccd51854f8a9ffbb72ed4e1ee087af734
stormvansoldt <stormvansoldt94@gmail.com>
2018-11-27 21:33:14 -0700

    updated documentation

--- Credit/credit.c
+++ Credit/credit.c
@@ -4,72 +4,13 @@
 #include <cs50.h>
 
 /*
- * AmEx: 15 digits, starts with 34 or 37
- * MasterCard: 16 digits
- * Visa: 13 OR 16 digits, starts with a 4
+ * Takes a card number from user input and validates it as either a Mastercard,
+ * VISA, or American Express using Luhn's algorithm and vendor attributes (ie,
+ * number of digits in card number, first one/two digits).
  */
 
-/*
- * Takes some arbitrary integer as an input and return the length of said integer.
- * Simply checks if the input is less than the length of the current multiplier (some multiple of
- * 10). If it is, return the length (starting at 1). Otherwise, increment length by one and
- * multiply the check int by 10.
- */
-int get_int_length(long num)
-{
-	int result = 1;
-	long multiplier = 10;		// This will be multiplied by 10 for every iteration
-								// of the loop
-
-	while (1 == 1)
-	{
-		if (num < multiplier)
-		{
-			return result;
-		}
-
-		result += 1;
-		multiplier *= 10;
-	}
-
-	return -1;
-}
-
-/*
- * Assuming the card number has already passed Luhn's algorithm, start checking the digits.
- * If the length is 15 and first two digits are 3,4 or 3,7, it's an AmEx. If the length
- * is 16 or 13 and card starts with 4, it's a Visa. Else if the card length is 16, it's a
- * Mastercard.
- *
- * Returns 1 for AmEx, 2 for MasterCard, 3 for Visa, or 0 for INVALID.
- */
-int check_card_type(long card, int len)
-{
-	int first_two = card / (pow(10, (len - 2)));
-
-	if (len == 15)
-	{
-        if (first_two == 34 || first_two == 37)
-		{
-            return 1;
-        }
-	}
-
-    if (len == 16 && first_two >= 51 && first_two <= 55)
-    {
-        return 2;
-    }
-
-    if (first_two / 10 == 4)
-	{
-        if (len == 16 || len == 13)
-        {
-            return 3;
-        }
-	}
-
-    return 0;
-}
+int get_int_length(long num);
+int check_card_type(long card, int len);
 
 int main(void)
 {
@@ -117,19 +58,92 @@ int main(void)
         return 0;
     }
 
-    switch (check_card_type(card_num, card_length))
-    {
-        case 1:
-            printf("AMEX\n");
-            break;
-        case 2:
-            printf("MASTERCARD\n");
-            break;
-        case 3:
-            printf("VISA\n");
-            break;
-        default:
-            printf("INVALID\n");
+    switch (check_card_type(card_num, card_length)) {
+    case 1:
+        printf("AMEX\n");
+        break;
+    case 2:
+        printf("MASTERCARD\n");
+        break;
+    case 3:
+        printf("VISA\n");
+        break;
+    default:
+        printf("INVALID\n");
     }
 
+}
+
+/*
+ * Function: get_int_length
+ * ------------------------
+ *  Takes an arbitrary integer as an input and return the length of said integer.
+ *  Simply checks if the input is less than the length of the current multiplier
+ *  (some multiple of 10). If so, return the length (starting at 1). Otherwise, 
+ *  increment length by one and multiply the check int by 10.
+ *
+ *  num: a positive integer
+ *
+ *  returns: length of the provided integer
+ */
+int get_int_length(long num)
+{
+    int result = 1;
+    long multiplier = 10;
+
+    while (1 == 1)
+    {
+        if (num < multiplier)
+        {
+            return result;
+        }
+
+        result += 1;
+        multiplier *= 10;
+    }
+
+    return -1;
+}
+
+/*
+ * Function: check_card_type
+ * -------------------------
+ *  Accepts a card number and the number of digits in the card number as inputs
+ *  and returns the type of card that was provided.
+ *  
+ *  AMEX: Length is 15, first two digits are 34 or 37
+ *  VISA: Length is 13 or 16, first digit is 4
+ *  MASTERCARD: length is 16, first two digits are generally 51-55
+ *
+ *  card: a card number of type long
+ *  len: an integer representing the number of digits in the card number
+ *
+ *  returns: 1 for AmEx, 2 for MasterCard, 3 for Visa, or 0 for INVALID.
+ */
+int check_card_type(long card, int len)
+{
+    int first_two = card / (pow(10, (len - 2)));
+
+    if (len == 15)
+    {
+        if (first_two == 34 || first_two == 37)
+        {
+            return 1;
+        }
+    }
+
+    if (len == 16 && first_two >= 51 && first_two <= 55)
+    {
+        return 2;
+    }
+
+    if (first_two / 10 == 4)
+    {
+        if (len == 16 || len == 13)
+        {
+            return 3;
+        }
+    }
+
+    return 0;
 }
